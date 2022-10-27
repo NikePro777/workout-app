ca585af6594b8a93397e8d7825170349fe628243
Evgeniy Datskij <evgenij.datskij@mail.ru>
2022-10-27 23:06:17 +0500

    setting plugins

---
+++ back/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import morgan from "morgan";
+import dotenv from "dotenv";
+import colors from "colors";
+
+// Routes
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config(); // чтобы эта хрень заработала
+
+const app = express(); // запускаем приложение
+
+if (process.env.NODE_ENV === "development") app.use(morgan("dev")); // если мы в режиме разработки - запускаем морган
+
+app.use(express.json()); // это чтобы ответ в формате JSON нам пришел
+
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold // желтым жирным в консоли запустится
+  )
+);
