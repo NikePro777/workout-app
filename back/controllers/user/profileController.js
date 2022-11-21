159b2bf955e4918d9804256fd8cba9be7cf838a4
Evgeniy Datskij <evgenij.datskij@mail.ru>
2022-11-21 22:10:44 +0500

    add exercise times function

--- back/controllers/user/profileController.js
+++ back/controllers/user/profileController.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler";
+import ExerciseLog from "../../models/exerciseLogModel.js";
 import User from "../../models/userModel.js";
 // @desc Get user profile
 // @route Get /api/users/profile
@@ -6,5 +7,14 @@ import User from "../../models/userModel.js";
 
 export const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id).select("-password");
-  res.json(user);
+
+  const execiseLogByUser = await ExerciseLog.find({
+    user: user._id,
+    completed: true,
+  }); // получили лог выполненного упражнения
+  let countExerciseTimesCompleted = 0;
+  execiseLogByUser.forEach((log) => {
+    countExerciseTimesCompleted += log.times.length;
+  });
+  res.json(countExerciseTimesCompleted);
 });
