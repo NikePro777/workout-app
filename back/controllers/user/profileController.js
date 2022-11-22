2886583316736d80ee4c05607aabcf5a3e983d70
Evgeniy Datskij <evgenij.datskij@mail.ru>
2022-11-22 07:51:48 +0500

    add count minutes and weight

--- back/controllers/user/profileController.js
+++ back/controllers/user/profileController.js
@@ -1,20 +1,32 @@
 import asyncHandler from "express-async-handler";
 import ExerciseLog from "../../models/exerciseLogModel.js";
 import User from "../../models/userModel.js";
+import WorkoutLog from "../../models/workoutLogModel.js";
 // @desc Get user profile
 // @route Get /api/users/profile
 // @accets Private
 
 export const getUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id).select("-password");
+  const user = await User.findById(req.user._id).select("-password").lean();
 
   const execiseLogByUser = await ExerciseLog.find({
     user: user._id,
     completed: true,
   }); // получили лог выполненного упражнения
+
   let countExerciseTimesCompleted = 0;
+  let kgs = 0;
+
   execiseLogByUser.forEach((log) => {
     countExerciseTimesCompleted += log.times.length;
+    log.times.forEach((item) => {
+      kgs += item.weight;
+    });
   });
-  res.json(countExerciseTimesCompleted);
+  const minutes = Math.ceil(countExerciseTimesCompleted * 2.3);
+  const workouts = await WorkoutLog.find({
+    user: user._id,
+    completed: true,
+  }).countDocuments(); // countDocuments - высчитывает количество ( крч длина взятого обьекта)
+  res.json({ ...user, minutes, workouts, kgs });
 });
