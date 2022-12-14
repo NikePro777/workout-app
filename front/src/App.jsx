91df32a1cf3dc896758e24fdc0c7555ed4c8c1c6
Evgeniy Datskij <evgenij.datskij@mail.ru>
2022-12-14 09:56:29 +0500

    i rrd add base routing

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,9 +1,25 @@
 import React from "react";
 // import Layout from "./components/common/Layout.jsx";
 import Home from "./components/pages/Home/Home";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import NewWorkout from "./components/pages/NewWorkout/NewWorkout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="">
+      <Route index element={<Home />} />
+      <Route path="/new-workout" element={<NewWorkout />} />
+    </Route>
+  )
+);
 
 const App = () => {
-  return <Home />;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ front/src/components/pages/NewWorkout/NewWorkout.jsx
@@ -0,0 +1,7 @@
+import styles from "./NewWorkout.scss";
+
+const NewWorkout = () => {
+  return <div>New Workout</div>;
+};
+
+export default NewWorkout;
