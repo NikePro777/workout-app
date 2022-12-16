88e332bb4144745da44a6ca6e833506a9662e998
Evgeniy Datskij <evgenij.datskij@mail.ru>
2022-12-16 08:50:30 +0500

    edit menu image and add style humburger

--- front/src/components/common/Header/Header.jsx
+++ front/src/components/common/Header/Header.jsx
@@ -3,6 +3,7 @@ import styles from "./Header.module.scss";
 import userImage from "../../../images/header/user.svg";
 
 import Humburger from "./Humburger/Humburger";
+
 const Header = () => {
   return (
     <header className={styles.header}>
--- front/src/components/common/Header/Humburger/Humburger.jsx
+++ front/src/components/common/Header/Humburger/Humburger.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import hamburgerImage from "../../../../images/header/hamburger.svg";
+import hamburgerCloseImage from "../../../../images/header/hamburger-close.svg";
 import styles from "./Humburger.module.scss";
 import { menu } from "./menuBase";
 
@@ -13,17 +14,17 @@ const Humburger = () => {
   return (
     <div className={styles.wrapper}>
       <button type="button" onClick={() => setShow(!show)}>
-        <img src={hamburgerImage} alt="Auth" />
+        <img src={show ? hamburgerCloseImage : hamburgerImage} alt="Auth" />
       </button>
-      <nav className={styles.menu}>
+      <nav className={`${styles.menu} ${show ? styles.show : ""}`}>
         <ul>
-          {menu.map((item) => (
-            <li>
+          {menu.map((item, idx) => (
+            <li key={`_menu_${idx}`}>
               <Link to={item.link}>{item.title}</Link>
             </li>
           ))}
           <li>
-            <a onClick={handleLogout}>Logout</a>
+            <button onClick={handleLogout}>Logout</button>
           </li>
         </ul>
       </nav>
