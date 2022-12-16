1fe266c3b0ef5beb93147ce68e33de86ff8d4aa0
Evgeniy Datskij <evgenij.datskij@mail.ru>
2022-12-16 13:31:30 +0500

    create component Field, add styles

--- front/src/components/common/Header/Header.jsx
+++ front/src/components/common/Header/Header.jsx
@@ -1,15 +1,31 @@
 import React from "react";
-import styles from "./Header.module.scss";
-import userImage from "../../../images/header/user.svg";
-
 import Humburger from "./Humburger/Humburger";
+import styles from "./Header.module.scss";
+
+import userImage from "../../../images/header/user.svg";
+import arrowImage from "../../../images/header/arrow.svg";
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 
 const Header = () => {
+  const { location } = useLocation();
+  const navigate = useNavigate();
   return (
     <header className={styles.header}>
-      <button type="button">
-        <img src={userImage} alt="Auth" />
-      </button>
+      {location?.pathname !== "/" ? (
+        <button
+          type="button"
+          onClick={() => {
+            navigate(-1);
+          }}
+        >
+          <img src={arrowImage} alt="Back" />
+        </button>
+      ) : (
+        <button type="button">
+          <img src={userImage} alt="Auth" />
+        </button>
+      )}
+
       <Humburger />
     </header>
   );
--- front/src/components/pages/NewWorkout/NewWorkout.jsx
+++ front/src/components/pages/NewWorkout/NewWorkout.jsx
@@ -1,7 +1,32 @@
-import styles from "./NewWorkout.scss";
+import Layout from "../../common/Layout";
+import styles from "./NewWorkout.module.scss";
+import bgImage from "../../../images/new-workout-bg.jpg";
+import Field from "../../ui/Field/Field";
+import { useState } from "react";
+import Button from "../../ui/Button/Button";
 
 const NewWorkout = () => {
-  return <div>New Workout</div>;
+  const [name, setName] = useState("");
+
+  const handleSubmit = () => {
+    console.log("submit");
+  };
+
+  return (
+    <>
+      <Layout bgImage={bgImage}></Layout>
+      <div className={styles.wrapper}>
+        <form onSubmit={handleSubmit}>
+          <Field
+            placeholder="Введите имя"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <Button text="Create" callback={() => {}} />
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default NewWorkout;
