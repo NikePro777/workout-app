411dff92780dba107f9ca1347ff85302c837bc67
Evgeniy Datskij <evgenij.datskij@mail.ru>
2023-02-09 09:11:00 +0500

    install and settigs  prisma pg and more

---
+++ server.js
@@ -0,0 +1,29 @@
+import "colors"
+import dotenv from "dotenv"
+import express from "express"
+import morgan from "morgan"
+
+import authRoutes from "./app/auth/auth.routes.js"
+
+dotenv.config() //хрень без которой наш файлик env работать не будет
+
+const app = express()
+
+async function main() {
+	if (process.env.NODE_ENV === "development") app.use(morgan("dev"))
+	// работает благодаря тому что сверху dotenv.config подключили, запустили морган - чтобы логи просматривались
+
+	const PORT = process.env.PORT || 5000
+
+	app.listen(
+		PORT,
+		console.log(
+			`Сервер запустился в режиме ${process.env.NODE_ENV} на порте ${PORT}`.blue
+				.bold
+		)
+	)
+	app.use(express.json()) // чтобы все данные входящие и исходящие были в формате json
+	app.use("/api/auth", authRoutes)
+}
+
+main()
